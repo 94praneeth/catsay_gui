023a059b6084fbcba12ca8c5ec943605cfa45efc
94praneeth <94praneeth@gmail.com>
2022-01-30 00:36:52 +0530

    Modified interface and completed the project

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,86 @@
+extern crate gio;
+extern crate gtk;
+
+use gio::prelude::*;
 use gtk::prelude::*;
-use gtk::{Application, ApplicationWindow, Box, Image, Label, Orientation};
 
-fn main() {
-    let app = Application::builder()
-        .application_id("org.PCR.catsay")
-        .build();
 
-    app.connect_activate(|app| {
-        // Creates the main window.
-        let win = ApplicationWindow::builder()
-            .application(app)
-            .default_width(320)
-            .default_height(200)
-            .title("Catsay")
-            .build();
+fn build_ui(app: &gtk::Application) {
+    let glade_src = include_str!("layout.glade");
+    let builder = gtk::Builder::from_string(glade_src);
 
-        // Here Box is not the std Rust Box type. This is a container
-        // from GTK library. It works as a layout that contains and align
-        // widgets.
-        let layout_box = Box::new(Orientation::Vertical, 0);
-        
-        // Creating a label.
-        let label = Label::new(Some("Meow!\n    \\\n      \\"));
-        // Adding the lable to layout.
-        layout_box.add(&label);
+    let window: gtk::Window = builder
+        .object("applicationwindow1").unwrap();
+    
+    window.set_application(Some(app));
 
-        // Getting the image into the program.
-        let cat_image = Image::from_file("./images/cat.png");
-        // Adding image into layout
-        layout_box.add(&cat_image);
+    // Load the input outputs after setting a the application.
 
-        // After all we have to add the layout onto the window.
-        win.add(&layout_box);
+    // Inputs.
+    let message_input: gtk::Entry = builder.object("message_input").unwrap();
 
-        // Making everything created visible.
-        win.show_all();
+    // Submit button.
+    let button: gtk::Button = builder.object("generate_button").unwrap();
+    let is_dead_switch: gtk::Switch = builder.object("is_dead_switch").unwrap();
 
+    // Outputs.
+    let message_output: gtk::Label = builder.object("message_output").unwrap();
+    let image_output: gtk::Image = builder.object("image_output") .unwrap();
+    let image_output_clone = image_output.clone();
+
+    button.connect_clicked(move |_| {
+        message_output.set_text(&format!(
+            "{}\n     \\\n    \\",
+            message_input.to_string().as_str()
+        ));
+
+        let is_dead = is_dead_switch.is_active();
+
+        if is_dead {
+            image_output_clone.set_from_file(Some("./images/cat_dead.png"));
+        } else {
+            image_output_clone.set_from_file(Some("./images/cat.png"));
+        }
+
+        image_output_clone.show();
     });
 
-    app.run();
+    window.show_all();
+    image_output.hide();
+
+}
+
+fn main() {
+
+    let application = gtk::Application::new(
+        Some("pcr.catsay-gui"),
+        Default::default()
+    );
+
+    application.connect_activate(|app|{
+        build_ui(app);
+    });
+
+    application.run();
+
+
+    // let app = Application::new(Some("pcr.catsay-gui"), gio::ApplicationFlags::empty());
+
+    // // Callback when the app starts
+    // app.connect_startup(|app| {
+    //     let window = ApplicationWindow::new(app);
+    //     window.set_title("Catsay");
+    //     window.set_default_size(350, 70);
+
+    //     window.connect_delete_event(|win, _| {
+    //         unsafe{
+    //             win.destroy();
+    //             Inhibit(false)
+    //         }
+    //     });
+    //     window.show_all();
+    // });
+
+    // app.connect_activate(|_|{});
+    // app.run();
 }
